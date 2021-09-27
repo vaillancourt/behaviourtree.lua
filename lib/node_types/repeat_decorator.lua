0bc7e47235b6b433dbd57745f5570a644410690e
Alexandre Vaillancourt <commitav@gmail.com>
2021-09-27 16:53:56 -0400

    Removing leftover debug code.

--- lib/node_types/repeat_decorator.lua
+++ lib/node_types/repeat_decorator.lua
@@ -5,15 +5,11 @@ local RepeatDecorator = class('RepeatDecorator', Decorator)
 
 
 function RepeatDecorator:start(object)
-  print("RepeatDecorator:start() " .. self.name .. " isObject=" .. ((object and "yes") or "no"))
-
   self:setObject(object)
   Decorator.start(self, object)
 end
 
 function RepeatDecorator:success()
-  print("RepeatDecorator:success() " .. self.name)
-
   self.node:finish(self.object)
   self.node:start(self.object)
 
@@ -21,8 +17,6 @@ function RepeatDecorator:success()
 end
 
 function RepeatDecorator:fail()
-  print("RepeatDecorator:fail() " .. self.name)
-
   self.control:success()
 end
 
